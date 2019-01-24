733f62de0e95f722581c28eb8ea323d33df25a91
Pascal <giraud.pascal@outlook.com>
2019-01-24 11:33:03 +0100

    vue index structure + css

--- src/index.php
+++ src/index.php
@@ -1,1 +1,50 @@
-<?php= 'Bonjour' ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+    </script>
+    <title>Index</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            Test de sélection pour la candidature à la formation RIL
+            </div>
+        </div>
+    </div> 
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3">
+            <a href="#"><input type="button" value="Commencer le test"/></a>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-3">
+            <a href="#"><input type="button" value="Accès recruteur"/></a>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @Copyright CESI - Janvier 2019
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
