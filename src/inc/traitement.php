eb26cf23d2e8d9f980d1d1c66b7c307ec800d4b1
Pascal <giraud.pascal@outlook.com>
2019-01-26 18:35:40 +0100

    insertion radio et chckbx + css dans traitement.php

--- src/inc/traitement.php
+++ src/inc/traitement.php
@@ -1,15 +1,21 @@
+<style><?php include('../css/traitement.css') ?></style>
 <?php include('../config.php');
 
 $q = new BDD;
 /* Afichage liste questions */
 $questions = $q->listeQuestions();
+$compteur = 0;
 foreach($questions as $valueQ) {
-    echo '<input name="questionID" type="hidden" value="'.$valueQ['id_question'].'">'.$valueQ['question'].'<br>';
+    echo ++$compteur.' - <input name="questionID" type="hidden" value="'.$valueQ['id_question'].'">'.$valueQ['question'].'<br>';
 
     /* Affichage liste réponses */
     $reponses = $q->listeReponses($valueQ['id_question']);
     foreach ($reponses as $valueR) {
-      echo '<input name="reponseID" type="hidden" value="'.$valueR['id_reponse'].'">'.$valueR['texte'].'<br>';
+      if ($valueR['type']=="radio") {
+        echo '<input class="tabReponses" type="radio" name="reponseID" type="hidden" value="'.$valueR['id_reponse'].'">&nbsp;&nbsp;'.$valueR['texte'].'<br>';
+      }else{
+        echo '<input class="tabReponses" type="checkbox" name="reponseID" type="hidden" value="'.$valueR['id_reponse'].'">&nbsp;&nbsp;'.$valueR['texte'].'<br>';
+      }
     }
-}
+    echo '<br>';}
 ?>
