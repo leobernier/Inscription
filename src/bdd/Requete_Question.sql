00ecc71cc2ba596b11552a82233dbcb469c38c79
Pascal <giraud.pascal@outlook.com>
2019-01-26 14:59:55 +0100

    bdd
    regroupement requêtes

---
+++ src/bdd/Requete_Question.sql
@@ -0,0 +1,9 @@
+INSERT INTO `questions`(`question`) VALUES ('Le nombre binaire 1011 vaut en décimal :'),
+('Quelle(s) est (sont) la ou les langue(s) officielle(s) aux Etats-Unis ?'),
+('En quelle année a été construite la tour Eiffel ?'),
+('Quelle est la hauteur de la tour Eiffel ?'),
+('Quelle(s) est (sont) la ou les langues officielle(s) au Canada'),
+('Quelle est la monnaie du Cambodge ?'),
+('Combien l ONU reconnait il d Etats dans le monde ?'),
+('Combien l ONU reconnait il d Etats indépendants dans le monde ?'),
+('Combien d Etats sont reconnaus à travers le monde par le Canada ?')
---
+++ src/bdd/Requete_Reponse.sql
@@ -0,0 +1,49 @@
+INSERT INTO `reponses`( `id_question`,`juste`, `texte`, `type`) VALUES (1,0,'152','radio'),
+(1,0,'100','radio'),
+(1,0,'150','radio'),
+(1,1,'151','radio'),
+
+(2,0,'7','radio'),
+(2,0,'6','radio'),
+(2,0,'3','radio'),
+(2,1,'11','radio'),
+
+(3,0,'Espagnole','checkbox'),
+(3,0,'Francais','checkbox'),
+(3,0,'Anglais','checkbox'),
+(3,1,'Il n y en a pas','checkbox'),
+
+(4,1,'1887','radio'),
+(4,0,'1888','radio'),
+(4,0,'1889','radio'),
+(4,0,'1890','radio'),
+
+(5,0,'400 mètres','radio'),
+(5,0,'350 mètres','radio'),
+(5,1,'300 mètres','radio'),
+(5,0,'200 mètres','radio'),
+
+(6,0,'Inuit','checkbox'),
+(6,1,'Francais','checkbox'),
+(6,0,'Langue autochtone','checkbox'),
+(6,1,'Anglais','checkbox'),
+
+(7,0,'Dong','radio'),
+(7,1,'Riel','radio'),
+(7,0,'Yen','radio'),
+(7,0,'Ringgit','radio'),
+
+(8,1,'194','radio'),
+(8,0,'193','radio'),
+(8,0,'195','radio'),
+(8,0,'192','radio'),
+
+(9,0,'194','radio'),
+(9,1,'193','radio'),
+(9,0,'195','radio'),
+(9,0,'192','radio'),
+
+(10,0,'194','radio'),
+(10,0,'193','radio'),
+(10,1,'195','radio'),
+(10,0,'192','radio')
